e892997ef86c19118605cf9976b2e0a7cd2bc143
Bruxe-Dev <mnibeza23@gmail.com>
2026-01-19 20:58:56 +0200

    added a way to calculate the total amount before the data is asaved this will help make the working with orders more efficiently

--- models/Cart.js
+++ models/Cart.js
@@ -34,3 +34,15 @@ const cartSchema = new mongoose.Schema({
     timeStamps: true
 
 });
+
+//Calculate the total before saving 
+
+cartSchema.pre('save', async function (next) {
+    this.totalAmount = this.items.reduce((total, item) => {
+        return total + (item.price * item.quantity)
+    }, 0);
+
+    next();
+});
+
+module.exports = mongoose.model('Cart', cartSchema)

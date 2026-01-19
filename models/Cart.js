682b0b0b33f5399bec312aaae71e472ec80c6323
Bruxe-Dev <mnibeza23@gmail.com>
2026-01-19 20:42:35 +0200

    finished the cart module with all the required fields

--- models/Cart.js
+++ models/Cart.js
@@ -1,5 +1,19 @@
 const { default: mongoose, mongo } = require('mongoose');
 
 const cartSchema = new mongoose.Schema({
-
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        min: 1,
+        default: 1
+    },
+    price: {
+        type: Number,
+        required: true
+    }
 });

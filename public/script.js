d90c3c13bae706dfd6ec4c7e2f64b12abefdbfc2
Bruxe-Dev <mnibeza23@gmail.com>
2026-01-23 10:44:48 +0200

    started to make the response struct5ure of Fronted add to cart

--- public/script.js
+++ public/script.js
@@ -1,45 +1,28 @@
-const API_BASE = 'http:/ / localhost: 5050 / api';
-const productsContainer = document.getElementById('products-container');
+const API_URI = 'http://localhost:5001/api';
+const getToken = () => localStorage.getItem('token');
 
-// Fetch products from backend
-async function fetchProducts() {
+//=========================Cart Functions====================//
+
+//1.Add To Cart
+
+async function addToCart(productId, quantity = 1) {
     try {
-        const response = await fetch(`${API_BASE}/products`);
-        const data = await response.json();
+        const token = getToken();
 
-        if (!data.success) {
-            productsContainer.innerHTML = '<p>Failed to load products</p>';
+        if (!token) {
+            alert('Please login to add to token');
+            window.location.href = '/login.html';
             return;
         }
-
-        // Clear existing content
-        productsContainer.innerHTML = '';
-
-        // Loop through each product
-        data.data.forEach(product => {
-            const col = document.createElement('div');
-            col.classList.add('col-md-4', 'mb-4');
-
-            col.innerHTML = `
-                <div class="card product-card">
-                    <img src="${product.imageUrl}" class="product-image card-img-top" alt="${product.name}">
-                    <div class="card-body">
-                        <h5>${product.name}</h5>
-                        <p class="product-description">${product.description}</p>
-                        <p class="product-price">${product.price} ${product.currency}</p>
-                        <button class="add-btn btn btn-primary"><i class="fas fa-shopping-cart"></i> Add</button>
-                    </div>
-                </div>
-            `;
-
-            productsContainer.appendChild(col);
+        const response = await fetch(`${API_URI}/cart/add`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorisation': `Bearer ${token}`
+            },
+            body: JSON.stringify({ productId, quantity })
         });
+    } catch (error) {
 
-    } catch (err) {
-        console.error('Error fetching products:', err);
-        productsContainer.innerHTML = '<p>Error loading products</p>';
     }
 }
-
-// Load products when page loads
-window.addEventListener('DOMContentLoaded', fetchProducts);
